59cd1fbcb797dacb529eff7f5395d8c4a52ca31a
Spa Pas <jboadi@spepas.com>
2025-08-12 17:41:49 +0200

    env fixed

--- src/libs/kafka/connection.ts
+++ src/libs/kafka/connection.ts
@@ -1,7 +1,7 @@
-// import { config } from '@useractivity/config';
+import { config } from '@useractivity/config';
 import { Kafka } from 'kafkajs';
 
 export const kafkaconnection = new Kafka({
   clientId: 'user-activity-service',
-  brokers: ['128.251.117.108:9094', '4.231.138.130:9095', '20.93.57.0:9096']
+  brokers: [config.BROKER1_URL!, config.BROKER2_URL!, config.BROKER3_URL!]
 });
